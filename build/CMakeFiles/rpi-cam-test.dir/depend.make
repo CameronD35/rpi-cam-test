dc432b3a70391762373e0d155b7213d12cbe85c5
CameronD35 <camster09@ca.rr.com>
2026-02-03 19:03:35 -0800

    nahhh it didn't

--- build/CMakeFiles/rpi-cam-test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for rpi-cam-test.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rsx/Desktop/software/rpi-cam-test
-# Build directory: /home/rsx/Desktop/software/rpi-cam-test/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
